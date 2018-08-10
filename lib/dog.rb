6325f4627830cfd0b06c7c737447b2cf777d0823
josephinem11 <josephine.man0112@yahoo.com>
2018-08-10 14:53:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# dog.rb
+class Dog
+  def initialize (breed)
+    @breed = breed
+  end
+  def breed 
+    @breed = 
+  end
+end
+  
+
+#   def name
+#     @new_name
+#   end
+# end
+#   def breed = (dog_type)
+#     @new_type = dog_type
