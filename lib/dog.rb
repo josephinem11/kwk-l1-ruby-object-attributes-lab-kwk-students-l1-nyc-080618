28ce5b0da35ade7219e7364346683628297f7ebe
josephinem11 <josephine.man0112@yahoo.com>
2018-08-10 14:54:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
-class Dog
-  def initialize (breed)
-    @breed = breed
-  end
-  def breed 
-    @breed = 
-  end
-end
+# class Dog
+#   def initialize (breed)
+#     @breed = breed
+#   end
+#   def breed 
+#     @breed = 
+#   end
+# end
   
 
 #   def name
